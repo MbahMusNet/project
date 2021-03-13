100149b56241c7841d6b1864cd33159518866c49
Aulia Satrio <auliasw89@gmail.com>
2021-03-13 21:42:31 +0700

    adding and implementing URI & form validation library

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -16,30 +16,65 @@ class Front extends CI_Controller
 
 
 
-  public function tambah_artikel($param = NULL)
-  {
-    if ($param == "kirim") {
-      $post = $this->input->post();
+  public function tambah_artikel(){
 
-      if (!empty($post['title']) && !empty($post['author']) && !empty($post['content'])) {
-        $this->load->model('Post_model');
+    //get the "kirim" method using URI Library
+		$action = $this->uri->segment(3);
+		$this->load->helper('form');
 
-        $data = array(
-          'title' => $post['title'],
-          'author' => $post['author'],
-          'date' => date("Y-m-d H:i:s"),
-          'content' => $post['content']
-        );
+    //form validation load
+		$this->load->library('form_validation');
+		if($action=='kirim'){
+			$post = $this->input->post();			
 
-        $this->Post_model->create('tbl_post', $data);
-        $this->load->view('tambah_artikel_berhasil', $data);
-      } else {
-        $this->load->view('tambah_artikel_gagal');
-      }
-    } else {
-      $this->load->view('tambah_artikel');
-    }
-  }
+			/*if(!empty($post['title']) && !empty($post['author']) && !empty($post['content'])){
+				$this->load->model('Post_model');
+
+				$data = array(
+						'title' => $post['title'],
+						'author' => $post['author'],
+						'date' => date('Y-m-d'),
+						'content' => $post['content']
+					);
+
+				$this->Post_model->create('tbl_post',$data);
+				$this->load->view('tambah_artikel_berhasil',$data);
+			}
+			else{
+				$this->load->view('tambah_artikel_gagal',$data);
+			}*/
+      
+      //setting up form validation
+			$this->form_validation->set_rules('title', 'Judul Artikel', 'required');
+			$this->form_validation->set_rules('author', 'Penulis', 'required');
+			$this->form_validation->set_rules('content', 'Isi artikel', 'required');
+
+      //create a custom message to each error
+			$this->form_validation->set_message('required','%s masih kosong, silahkan diisi');
+			$this->form_validation->set_error_delimiters('<p class="alert">', '</p>');
+
+			if($this->form_validation->run() == FALSE){
+				$this->load->view('tambah_artikel');
+			}
+			else{
+				$this->load->model('Post_model');
+
+				$data = array(
+						'title' => $post['title'],
+						'author' => $post['author'],
+						'date' => date('Y-m-d'),
+						'content' => $post['content']
+					);
+
+				$this->Post_model->create('tbl_post',$data);
+				$this->load->view('tambah_artikel_berhasil',$data);		
+			}			
+
+		}
+		else{
+			$this->load->view('tambah_artikel');
+		}		
+	}
 
   public function daftar_artikel()
   {
