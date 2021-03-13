1eca4239ec20ab7ff731f050f2c0a49bbacbc0a7
Aulia Satrio <auliasw89@gmail.com>
2021-03-13 21:46:07 +0700

    edit the tambah artikel view

--- application/views/tambah_artikel.php
+++ application/views/tambah_artikel.php
@@ -1,46 +1,43 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
 <html lang="en">
+	<head>
+	<meta charset="utf-8">	
+	<title>Formulir Penambahan Artikel</title>
+	<style type="text/css">
+		label{
+			width:100px; display: inline-block; margin-bottom: 10px;
+		}
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tambah Artikel</title>
-  <style type="text/css">
-    label {
-      width: 100px;
-      display: inline-block;
-      margin-bottom: 10px;
-    }
+		input[type=text]{
+			width: 280px;
+		}
 
-    input[type=text] {
-      width: 280px;
-    }
+		textarea{
+			width: 280px; height: 140px; vertical-align: text-top;
+		}
 
-    textarea {
-      width: 280px;
-      height: 150px;
-      vertical-align: text-top;
-    }
-
-    input [type=submit] {
-      margin-top: 30px;
-      margin-left: 105px;
-    }
-  </style>
-</head>
-
-<body>
-  <h1>Form Tambah Artikel</h1>
-  <form method="POST" action="<?php echo base_url('front/tambah_artikel/kirim'); ?>">
-    <label>Judul</label><input type="text" name="title" /><br />
-    <label>Penulis</label><input type="text" name="author" /><br />
-    <label>Konten</label><textarea name="content"></textarea><br />
-    <input type="submit" name="submit" value="Tambah Artikel" />
-  </form>
-</body>
-
+		input[type=submit]{
+			margin-top: 10px; margin-left: 105px;
+		}
+		p.alert{color: red;}
+	</style>
+	</head>
+	<body>
+		<h1>Formulir Tambah Artikel</h1>
+		<p>Berikut adalah menu yang bisa diakses</p>
+		<a href="<?=base_url('front/tambah_artikel');?>">Tambah Artikel</a> | 
+		<a href="<?=base_url('front/daftar_artikel');?>">Daftar Artikel</a>
+		<br /><br />		
+	<?php // echo validation_errors(); ?>
+	<form action="<?php echo base_url("front/tambah_artikel/kirim");?>" method="POST">
+	<?php echo form_error('title'); ?>
+	<label>Judul Artikel:</label> <input type="text" name="title" value="<?php echo set_value('title');?>" /><br />
+	<?php echo form_error('author'); ?>
+	<label>Penulis:</label> <input type="text" name="author" value="<?php echo set_value('author');?>" /><br />
+	<?php echo form_error('content'); ?>
+	<label>Isi Artikel:</label> <textarea name="content"><?php echo set_value('content');?></textarea><br />
+	<input type="submit" name="submit" value="Tambah Artikel" />
+	</form>
+	</body>
 </html>
